45c74bcdc4da93fe976c7b72069f7df7a5fdbf63
lidafan.me <lidafan.me@bytedance.com>
2026-07-23 21:14:04 +0800

    feat: prevent zoom

---
+++ packages/web/src/preventMobileGestureZoom.ts
@@ -0,0 +1,16 @@
+const preventDefault = (event: Event): void => {
+  event.preventDefault();
+};
+
+const preventMultiTouchMove = (event: TouchEvent): void => {
+  if (event.touches.length > 1) {
+    event.preventDefault();
+  }
+};
+
+export const preventMobileGestureZoom = (): void => {
+  document.addEventListener("gesturestart", preventDefault, { passive: false });
+  document.addEventListener("gesturechange", preventDefault, { passive: false });
+  document.addEventListener("gestureend", preventDefault, { passive: false });
+  document.addEventListener("touchmove", preventMultiTouchMove, { passive: false });
+};
